2f3408bdfc0d786f92f7950f6e7a2037d57e911f
jkk290 <jared@kuromoto.dev>
2025-11-13 16:45:23 -1000

    work in progress - create a queue

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,6 +6,8 @@ import (
 	"os"
 	"os/signal"
 
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
@@ -13,10 +15,21 @@ func main() {
 	const connectionString = "amqp://guest:guest@localhost:5672/"
 	amqpConnection, err := amqp.Dial(connectionString)
 	if err != nil {
-		log.Fatalf("error creating amqp connection: %s", err)
+		log.Fatalf("error creating amqp connection: %v", err)
 	}
 	defer amqpConnection.Close()
 	fmt.Println("amqp connection successful!")
+	amqpCh, err := amqpConnection.Channel()
+	if err != nil {
+		log.Fatalf("error creating channel: %v", err)
+	}
+
+	if err := pubsub.PublishJSON(amqpCh, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{
+		IsPaused: true,
+	}); err != nil {
+		log.Fatalf("error publishing JSON: %v", err)
+	}
+
 	fmt.Println("Starting Peril server...")
 	signalChan := make(chan os.Signal, 1)
 	signal.Notify(signalChan, os.Interrupt)
